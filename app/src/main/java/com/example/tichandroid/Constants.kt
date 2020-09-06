0477b70304730b0a87040758de7053a11bbc945d
SeungYooon <tmddbs4998@naver.com>
2020-09-06 22:36:21 +0900

    set Constants using Util

---
+++ app/src/main/java/com/example/tichandroid/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.tichandroid
+
+const val BASE_URL = "http://13.209.222/10:8080"
+const val CONNECT_TIMEOUT: Long = 60
+const val READ_TIMEOUT: Long = 60
+const val WRITE_TIMEOUT: Long = 60
