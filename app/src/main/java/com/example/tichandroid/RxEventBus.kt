b1161809845031651bf58e57e942857b012aed2f
SeungYooon <tmddbs4998@naver.com>
2020-09-08 21:16:43 +0900

    set RxEventBus for passing value

---
+++ app/src/main/java/com/example/tichandroid/RxEventBus.kt
@@ -0,0 +1,17 @@
+package com.example.tichandroid
+
+import io.reactivex.subjects.PublishSubject
+
+object RxEventBus {
+
+    val subjectSetDate = PublishSubject.create<String>()
+    val subjectSetCycle = PublishSubject.create<String>()
+
+    fun setDate(date: String) {
+        subjectSetDate.onNext(date)
+    }
+
+    fun setCycle(cycle: String) {
+        subjectSetCycle.onNext(cycle)
+    }
+}
