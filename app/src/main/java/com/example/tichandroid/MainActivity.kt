1c5f2c1ddf7f790cd938beceb07ca116b84a7304
SeungYooon <tmddbs4998@naver.com>
2020-09-09 20:31:09 +0900

    modify view in MainActivity

--- app/src/main/java/com/example/tichandroid/MainActivity.kt
+++ app/src/main/java/com/example/tichandroid/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.tichandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import com.example.tichandroid.view.ui.SuppliesFragment
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_shaving.*
+import kotlinx.android.synthetic.main.cycle_bottom_sheet.*
 import kotlinx.android.synthetic.main.supplies_bottom_sheet.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,10 +22,27 @@ class MainActivity : AppCompatActivity() {
 
         bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
 
-        btn_choice.setOnClickListener {
+        btn_continue.setOnClickListener {
+            clickBtn()
+
+            Handler().postDelayed({
+                originBtn()
+            }, 1000)
+
             SuppliesFragment().apply {
                 show(supportFragmentManager, SuppliesFragment.TAG)
             }
         }
+    }
+
+    private fun originBtn() {
+        btn_continue.setTextColor(ContextCompat.getColor(baseContext, R.color.colorText))
+        btn_continue.setBackgroundResource(R.drawable.button_border)
+    }
+
+    private fun clickBtn() {
+        btn_continue.setBackgroundResource(R.drawable.button_click_border)
+        btn_continue.setTextColor(ContextCompat.getColor(this, R.color.colorWhite))
+
     }
 }
