304f51551d736a64bcb28f672bcf3bbc50b6ec6a
d2fault <d2fault@naver.com>
2020-10-02 21:27:34 +0900

    Add google login at WalkLastFragment

--- app/src/main/java/com/example/tichandroid/view/ui/WalkLastFragment.kt
+++ app/src/main/java/com/example/tichandroid/view/ui/WalkLastFragment.kt
@@ -1,19 +1,82 @@
 package com.example.tichandroid.view.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.tichandroid.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+
 
 class WalkLastFragment : Fragment() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_walk_last, container, false)
+        val view = inflater.inflate(R.layout.fragment_walk_last, container, false)
+        val buttonGoogleLogin: com.shobhitpuri.custombuttons.GoogleSignInButton =
+            view.findViewById(R.id.google_login)
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(view.context, gso)
+        auth = FirebaseAuth.getInstance()
+
+        buttonGoogleLogin.setOnClickListener {
+            signIn()
+        }
+        return view
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle email: " + account.email)
+                Log.d(TAG, "firebaseAuthWithGoogle idToken: " + account.idToken)
+                Log.d(TAG, "firebaseAuthWithGoogle displayName: " + account.displayName)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                Log.w(TAG, "Google sign in failed", e)
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                } else {
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                }
+            }
+    }
+
+    companion object {
+        private const val TAG = "WalkLastFragment"
+        private const val RC_SIGN_IN = 9001
     }
 }
